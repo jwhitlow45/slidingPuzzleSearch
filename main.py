31012f736ca2902c427d137e04c6df900ac9338e
jwhitlow45 <jwhitlow45@gmail.com>
2022-03-12 23:03:56 -0800

    feat:add method to print puzzle

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 def main():
     
+    print_puzzle("0123456789ABCDEF", 4)
+
     pass
 
+def print_puzzle(puzzle: str, size: int):
+    for i in range(len(puzzle)):
+        print(puzzle[i], end=' ')
+        if (i - size + 1) % size == 0:
+            print('')    
+    
 
 if __name__ == '__main__':
     main()
