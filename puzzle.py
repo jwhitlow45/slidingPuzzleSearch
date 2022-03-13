f89ed90dfa6aea1ae964dec793118f3ac066d436
jwhitlow45 <jwhitlow45@gmail.com>
2022-03-12 23:47:33 -0800

    feat:add warning message if coord or index conversion is out of puzzle bounds

--- puzzle.py
+++ puzzle.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 
 class Puzzle:
     def __init__(self, initState: str, goalState: str, size: int):
@@ -7,11 +7,14 @@ class Puzzle:
         self.size = size
     
         
-        
     def index_to_coord(self, index: int) -> Tuple[int, int]:
+        if index >= len(self.initState):
+            print('WARNING: OUT OF PUZZLE BOUNDS!')
         return (int(index/self.size), index%self.size)
     
     def coord_to_index(self, col: int, row: int) -> int:
+        if row >= self.size or col >= self.size:
+            print('WARNING: OUT OF PUZZLE BOUNDS!')
         return row * self.size + col
     
     def print_states(self):  
@@ -20,6 +23,9 @@ class Puzzle:
         print('Goal state:')
         Puzzle.print_puzzle(Puzzle, self.goalState, self.size)
         
+    def move(cls, curState: List[str], move: str):
+        pass
+    
     def print_puzzle(cls, puzzle: str, size: int):
         for i, tile in enumerate(puzzle):
             print(tile, end=' ')
