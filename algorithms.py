e3e009795a7f26d7319b230c9e93793e606f02e6
jwhitlow45 <jwhitlow45@gmail.com>
2022-03-31 16:50:09 -0700

    feat:add a star iterative search

--- algorithms.py
+++ algorithms.py
@@ -2,7 +2,7 @@ from typing import List, Tuple
 from puzzle import Puzzle
 from queue import PriorityQueue
 
-EXPANSION_LIMIT = 2000000
+EXPANSION_LIMIT = 3000000
 
 def breadth_first_search(initState: str, goalState: str, size: int) -> Tuple[str, int]:
     numExpansions = 0
@@ -75,10 +75,7 @@ def iterative_deepening_search(initState: str, goalState: str, size: int) -> Tup
             return (f'exceeded limit of {EXPANSION_LIMIT} expansions', numExpansions)
         
         depth, state, z, moves = stack.pop()
-        # print(depth, state, z, moves)
-        # if len(stack) > 100:
-        #     break
-        
+
         # check if solution has already been explored
         if ''.join(state) in visited:
             continue
@@ -126,7 +123,7 @@ def a_star_search(initState: str, goalState: str, size: int) -> Tuple[str, int]:
         
         # in this implementation of a*, depth = path cost because has a cost of
         # 1 since it is adding one step (l,r,u,d) to the final solution
-        _, depth, state, z, moves = pq.get()
+        _, pathCost, state, z, moves = pq.get()
         
         if ''.join(state) in visited:
             continue
@@ -139,10 +136,69 @@ def a_star_search(initState: str, goalState: str, size: int) -> Tuple[str, int]:
         # expand and add possible solutions to pq
         for newStateTuple in expand((state, z, moves), size):
             newState, newZ, newMoves = newStateTuple
-            # heuristic = manhattan_distance(newState, goalStateDict, size)
-            heuristic = out_of_place(newState, goalState)
-            pq.put((depth + 1 + heuristic, depth + 1, newState, newZ, newMoves))
+            heuristic = manhattan_distance(newState, goalStateDict, size)
+            # heuristic = out_of_place(newState, goalState)
+            pq.put((pathCost + 1 + heuristic, pathCost + 1, newState, newZ, newMoves))
         numExpansions += 1
+
+def a_star_search_iterative(initState: str, goalState: str, size: int) -> Tuple[str, int]:
+    numExpansions = 0
+    zeroIndex = initState.index('0')
+    initState = list(initState)
+    goalState = list(goalState)
+    depthLimit = 2
+
+    # dictionary representing goal state, provides O(1) lookup time for computing manhattan distance
+    # this is not strictly necessary but it does save a lot of time iterating over indicies
+    goalStateDict = {}
+    for i, tile in enumerate(goalState):
+        goalStateDict[tile] = i
+        
+    # priority queue contains following tuple structure
+    # (estimated cost [g(n)+h(n)], path cost, depth, state, 0 index, moves)
+    pq = PriorityQueue()
+    tempPQ = PriorityQueue()
+    pq.put((0, 0, 0, initState, zeroIndex, ""))
+    visited = {}
+    
+    while pq.qsize() > 0:
+        if numExpansions > EXPANSION_LIMIT:
+            return (f'Exceeded limit of {EXPANSION_LIMIT} expansions', numExpansions)
+        
+        # in this implementation of a*, depth = path cost because has a cost of
+        # 1 since it is adding one step (l,r,u,d) to the final solution
+        _, pathCost, depth, state, z, moves = pq.get()
+        
+        if ''.join(state) in visited and depth + 1 < depthLimit:
+            continue
+        visited[''.join(state)] = 1
+        
+        # check if we have found solution
+        if state == goalState:
+            return (moves, numExpansions)
+        
+        # expand and add possible solutions to pq
+        for newStateTuple in expand((state, z, moves), size):
+            newState, newZ, newMoves = newStateTuple
+            heuristic = manhattan_distance(newState, goalStateDict, size)
+            # heuristic = out_of_place(newState, goalState)
+            
+            # if we are within our depth limit then put state into priority queue
+            # as normal, otherwise we have exceeded our depth limit and the new
+            # state must be stored in a temp priority queue containing all nodes
+            # just past the set depth limit. this temp pq is accessed when all
+            # nodes within the current depth limit have been explored (pq is empty)
+            if depth + 1 < depthLimit:
+                pq.put((pathCost + 1 + heuristic, pathCost + 1, depth + 1, newState, newZ, newMoves))                
+            else:
+                tempPQ.put((pathCost + 1 + heuristic, pathCost + 1, 0, newState, newZ, newMoves))
+        
+        if pq.empty():
+            pq = tempPQ # move past current depth limit
+            tempPQ = PriorityQueue() # create new queue for next depth limit
+        
+        numExpansions += 1
+        
 
 def expand(stateTuple: Tuple[str, int, str], size: int) -> List[Tuple[str, int, str]]:
     # unpack for legibility
